b12436c1a2f0cda21db5a2105450810502b85922
Suraj Kamat <suraj@byldd.com>
2025-07-11 10:02:46 +0530

    fix: basic code structure

---
+++ index.js
@@ -0,0 +1,34 @@
+const app = require('express')();
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const fs = require("fs");
+const path = require("path");
+
+app.use(cors());
+app.use(bodyParser.json());
+
+
+app.get("/", (_, res) => {
+    return res.status(200).json({
+        message: "Welcome to the test server"
+    });
+})
+
+app.get("/user", (req, res) => {
+    const filePath = path.join(__dirname, "user_data.json");
+    fs.readFile(filePath, 'utf8', (err, data) => {
+        if (err) {
+            return res.status(500).json({ error: "Failed to read user data" });
+        }
+        try {
+            const userData = JSON.parse(data);
+            return res.status(200).json(userData);
+        } catch (parseError) {
+            return res.status(500).json({ error: "Failed to parse user data" });
+        }
+    });
+})
+
+app.listen(8000, () => {
+    console.log('Server is running on port 8000');
+});
